bc49ff1df759d1d6fc5ada8afc9a1c62c0698016
guanwill <w.guan@hotmail.com>
2017-02-05 21:54:36 +1100

    configured deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :scm, :git
 set :repo_url, "https://github.com/guanwill/what2eat.git"
 set :branch, 'master'
 set :rails_env, 'production'
-set :keep_releases, 5
+set :keep_releases, 3
 set :user, 'will'
 
 # Deploy config
@@ -25,18 +25,18 @@ namespace :deploy do
    end
 end
 
-namespace :deploy do
-  desc "Reload the database with seed data"
-  task :seed do
-    on roles(:app) do
-      execute "cd #{current_path}; bundle exec rake db:seed"
-    end
-  end
-
-  desc "Migrate the database"
-  task :migrate do
-    on roles(:app) do
-      execute "cd #{current_path}; bundle exec rake db:migrate"
-    end
-  end
-end
+# namespace :deploy do
+#   desc "Reload the database with seed data"
+#   task :seed do
+#     on roles(:app) do
+#       execute "cd #{current_path}; bundle exec rake db:seed"
+#     end
+#   end
+#
+#   desc "Migrate the database"
+#   task :migrate do
+#     on roles(:app) do
+#       execute "cd #{current_path}; bundle exec rake db:migrate"
+#     end
+#   end
+# end
